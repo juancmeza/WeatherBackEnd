ab2bf463be2149f97820b10cc6fa8407fbe23d3a
juan_carlos_meza <juancmezat@gmail.com>
2021-03-19 17:44:52 -0700

    key

--- app/models/user_location.rb
+++ app/models/user_location.rb
@@ -1,10 +1,12 @@
+require_relative '../.apiKey.rb'
+
 class UserLocation < ApplicationRecord
 
   belongs_to :user
 
   def self.get_request(latitude, longitude)
-    api = ENV["WEATHER_API_KEY"]
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{latitude}&lon=#{longitude}&units=imperial&exclude={part}&appid=#{api}"
+    # api = ENV["WEATHER_API_KEY"]
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{latitude}&lon=#{longitude}&units=imperial&exclude={part}&appid=#{$api_key}"
     JSON.parse(RestClient.get(url))
   end
 
