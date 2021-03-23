54569cd69b32c3610fe576150a5bb267ca3d66ab
juan_carlos_meza <juancmezat@gmail.com>
2021-03-22 23:58:33 -0700

    debugging

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :users
 
   get 'user_locations/render_request', to: 'user_locations#render_request'
-  post '/login', to: 'users#login'
+  post 'users/login', to: 'users#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
