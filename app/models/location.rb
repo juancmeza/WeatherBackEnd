3e47223b5e3a18029b3e0341da4b39f1d665a36f
juan_carlos_meza <juancmezat@gmail.com>
2021-06-02 21:09:22 -0700

    config vars attempt #2

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,8 +4,8 @@ Dotenv.load
 class Location < ApplicationRecord
 
   def self.get_request(latitude, longitude)
-    api = ENV["WEATHER_API_KEY"]
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{latitude}&lon=#{longitude}&units=imperial&exclude={part}&appid=#{WAPI}"
+    api = ENV["WAPI"]
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{latitude}&lon=#{longitude}&units=imperial&exclude={part}&appid=#{api}"
     JSON.parse(RestClient.get(url))
   end
 
