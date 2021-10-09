3126f8b36481b26908019480d3d6e2140691965c
Juan Carlos Meza <juancmezat@gmail.com>
2021-10-09 02:09:33 -0700

    Added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
 
   has_many :user_locations, :dependent => :destroy
 
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :password, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
 end
